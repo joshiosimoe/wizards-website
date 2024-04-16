cd7d1e29ab6dd118d25c9a5e6d3c551b37d36b44
Joshua Yustana <joshiosimoe@gmail.com>
2024-04-16 01:17:10 -0500

    Removed Images Test

--- src/app/components/WizardCards.tsx
+++ src/app/components/WizardCards.tsx
@@ -5,16 +5,16 @@ import Image from "next/image"
 const WizardCards = (props:any) => {
     return(
         <div className="bg-white h-[500px] rounded-md flex flex-col items-center p-10 gap-2 text-lg text-wrap">
-            <div className="grid place-items-center h-[300px]">
+            {/* <div className="grid place-items-center h-[300px]">
                 <Image
                 src={props.wizardURL}
                 alt={`${props.firstName}'s Wizard`}
                 width={200}
                 height={200}
                 />
-            </div>
+            </div> */}
             <div className="font-bold">
-                <span>{props.firstName ? props.firstName : "???"}</span>
+                <span>{props.firstName}</span>
             </div>
             <div>
                 <span>{props.pronouns}</span>

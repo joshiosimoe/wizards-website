5895537474aaae4a66d59d792e83c24b02a753f2
Joshua Yustana <joshiosimoe@gmail.com>
2024-04-16 01:18:32 -0500

    Added Images Back

--- src/app/components/WizardCards.tsx
+++ src/app/components/WizardCards.tsx
@@ -5,14 +5,14 @@ import Image from "next/image"
 const WizardCards = (props:any) => {
     return(
         <div className="bg-white h-[500px] rounded-md flex flex-col items-center p-10 gap-2 text-lg text-wrap">
-            {/* <div className="grid place-items-center h-[300px]">
+            <div className="grid place-items-center h-[300px]">
                 <Image
                 src={props.wizardURL}
                 alt={`${props.firstName}'s Wizard`}
                 width={200}
                 height={200}
                 />
-            </div> */}
+            </div>
             <div className="font-bold">
                 <span>{props.firstName}</span>
             </div>

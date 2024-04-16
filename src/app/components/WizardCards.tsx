d1ac9a5300c5612ea743ea0303031aa969f3e1c5
Joshua Yustana <joshiosimoe@gmail.com>
2024-04-16 01:26:45 -0500

    Test #4

--- src/app/components/WizardCards.tsx
+++ src/app/components/WizardCards.tsx
@@ -4,7 +4,7 @@ import Image from "next/image"
 
 const WizardCards = (props:any) => {
     return(
-        <div className="bg-white h-[500px] rounded-md flex flex-col items-center p-10 gap-2 text-lg text-wrap overflow-visible">
+        <div className="bg-white h-[500px] rounded-md flex flex-col items-center p-10 gap-2 text-lg text-wrap">
             <div className="grid place-items-center h-[300px]">
                 <Image
                 src={props.wizardURL}
@@ -13,14 +13,16 @@ const WizardCards = (props:any) => {
                 height={200}
                 />
             </div>
-            <div className="font-bold">
-                <span>{props.firstName}</span>
-            </div>
-            <div>
-                <span>{props.pronouns}</span>
-            </div>
-            <div>
-                <span>WE LUV WIZARDS!!</span>
+            <div className="h-[300px] bg-yellow-400">
+                <div className="font-bold bg-red-400">
+                    <span>{props.firstName}</span>
+                </div>
+                <div>
+                    <span>{props.pronouns}</span>
+                </div>
+                <div>
+                    <span>WE LUV WIZARDS!!</span>
+                </div>
             </div>
         </div>
     )
